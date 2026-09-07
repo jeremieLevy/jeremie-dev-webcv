0a9df5f4d80a84d99dd002e58f78e3e50f7d1643
jeremie <jeremylevy.131290@gmail.com>
2026-09-07 20:18:33 +0200

    fix: bypass faq section & bash command for mail contact

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,11 @@
+import ContainerTemplate from './ui/ContainerTemplate';
+
+export default function Footer() {
+  return (
+    <ContainerTemplate size="large">
+      <div className="py-10 border-t border-t-light/50">
+        <p className="text-sm">© Jeremie-dev 2026. Tous droits réservés</p>
+      </div>
+    </ContainerTemplate>
+  );
+}
--- src/components/common/navigation/NavigationData.ts
+++ src/components/common/navigation/NavigationData.ts
@@ -8,5 +8,5 @@ export const navItems: NavItem[] = [
   { id: 1, label: 'Experience', link: '#experiences' },
   { id: 2, label: 'Projects', link: '#projects' },
   { id: 3, label: 'Hobbies', link: '#graphic-portfolio' },
-  { id: 4, label: 'Faq', link: '#faq' },
+  // { id: 4, label: 'Faq', link: '#faq' },
 ];
